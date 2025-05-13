65866d3b7b89b57b7cf640d11c0a14b722976df6
talhanuhuymaz <talha_nuh@hotmail.com>
2025-05-13 17:05:36 +0300

    admin

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.signup, name='signup-page'),
-    path('loginn/', views.loginn, name='login-page'),
+    path('login/', views.login_view, name='login-page'),
     path('home/', views.home, name='home-page'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login-page'), name='logout'),
+    path('new-post/', views.new_post, name='new-post'),
+    path('my-posts/', views.my_posts, name='my-posts'),
 ]
