0681c7b5afaa304b98f270627712a95a22077886
sefassenturk <sefantrk17@outlook.com>
2025-05-15 18:52:16 +0300

    Home Page Updates and Syntax error fix

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Post, UserProfile
 from django.conf import settings
 import os
