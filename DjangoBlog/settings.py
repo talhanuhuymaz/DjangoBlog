119c3c0a7d6423da83efbec022b0caeba4228783
sefassenturk <sefantrk17@outlook.com>
2025-05-16 13:25:44 +0300

    Home and Profile Page Fix

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -42,7 +42,12 @@ TEMPLATES = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # ... other middleware ...
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 # Add this at the bottom of the file
