491658eef3f9711a279eef4bdc5b255471f6642d
talhanuhuymaz <talha_nuh@hotmail.com>
2025-05-18 22:47:41 +0300

    PostImage and UI
    Fixed the posting Image and also edited the looking of website

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.login_view, name='login-page'),
@@ -15,3 +17,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('change_password/', views.change_password, name='change_password'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
