a13da5aea337b007f3eae0fbfb6fd523c88d9e00
talhanuhuymaz <talha_nuh@hotmail.com>
2025-05-26 20:07:15 +0300

    General Fix
    Updated the follow button, Added a new toggleFollow JavaScript function, Updated the toggle_follow v,

--- blog/urls.py
+++ blog/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('like/', views.like_post, name='like-post'),
     path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
     path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path('toggle_follow/<str:username>/', views.toggle_follow, name='toggle_follow'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
