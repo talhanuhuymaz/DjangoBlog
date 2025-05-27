c8c44040204bb4aa176abe6db537cbebef787fde
talhanuhuymaz <talha_nuh@hotmail.com>
2025-05-27 13:05:46 +0300

    1
    1

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'djangoblog-hpon.onrender.com']
 
